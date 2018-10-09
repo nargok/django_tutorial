5d08553c14907205d3e3216b6dca7b7d4383be80
Ryoichi Gokan <nargok@gmail.com>
2018-10-10 07:57:33 +0900

    view関数でindex出力

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  # 山括弧を使用すると、URLの一部が「キャプチャ」され、キーワード引数としてビュー関数に送信します。
   path('<int:question_id>/', views.detail, name='detail'),
   path('<int:question_id>/results/', views.results, name='results'),
   path('<int:question_id>/vote/', views.vote, name='vote'),
