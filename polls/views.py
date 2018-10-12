9ef6f598aa96d6c8d9f81a2c5127d359712c8952
Ryoichi Gokan <nargok@gmail.com>
2018-10-13 07:25:14 +0900

    detailとresultsのビュー、テンプレートを実装

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,12 +17,20 @@ def detail(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
   return render(request, 'polls/detail.html', {'question': question})
 
-
-  # return HttpResponse("You are looking at question %s." % question_id)
-
 def results(request, question_id):
-  response = "You are looking at the resulsts of question %s."
-  return HttpResponse(response % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-  return HttpResponse("You are voting on question %s." % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except(KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "You didn't select a choice"
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
