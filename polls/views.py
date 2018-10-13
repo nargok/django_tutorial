16b0f2adb3d7d65823a70013b8b592a9a3c95ab5
Ryoichi Gokan <nargok@gmail.com>
2018-10-13 21:06:13 +0900

    IndexViewのテストを実装

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -11,8 +12,11 @@ class IndexView(generic.ListView):
   context_object_name = 'latest_question_list'
 
   def get_queryset(self):
-    """Return the latest five published question."""
-    return Question.objects.order_by('-pub_date')[:5]
+    """
+    Return the latest five published question.
+    (not including those set to be published in the future)
+    """
+    return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
   model = Question
