d9e1c993f3a895c6ccd8fe48c646fc55276ef396
Ryoichi Gokan <nargok@gmail.com>
2018-10-17 21:52:03 +0900

    共通パーツを作成

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
