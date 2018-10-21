b2329de6f44cdde7168a46861880ded315d11196
Ryoichi Gokan <nargok@gmail.com>
2018-10-21 11:19:13 +0900

    ユーザー登録機能

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
