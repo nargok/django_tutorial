4798806e7ccd0d82089a62b73987c2cf31e57869
Ryoichi Gokan <nargok@gmail.com>
2018-10-21 11:26:54 +0900

    ユーザ登録後、自動的にログインする

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -5,5 +6,11 @@ from .forms import SignupForm
 
 class SignupView(CreateView):
   form_class = SignupForm
-  success_url = reverse_lazy('login')
+  success_url = reverse_lazy('home')
   template_name = 'registration/signup.html'
+
+  def form_valid(self, form):
+    # self.objectにsave()されたユーザーオブジェクトが格納される
+    valid = super().form_valid(form)
+    login(self.request, self.object)
+    return valid
